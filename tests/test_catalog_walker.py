eceb2477fa75432cc21e7c5e9952b69d309752f4
milosz.hoc <miloszhoc@gmail.com>
2019-09-11 23:09:26 +0200

    added tests

---
+++ tests/test_catalog_walker.py
@@ -0,0 +1,6 @@
+import unittest
+import catalog_walker
+
+
+class TestCatalogWalker(unittest.TestCase):
+    pass
---
+++ tests/test_comment_sign.py
@@ -0,0 +1,18 @@
+import pytest
+import comment_sign
+from exceptions.exceptions import *
+
+
+def test_ext_to_comment_sign_when_sign_is_in_dict_in_tuple():
+    sign = comment_sign.ext_to_comment_sign('.c')
+    assert sign == '//'
+
+
+def test_ext_to_comment_sign_when_sign_is_in_dict_as_str():
+    sign = comment_sign.ext_to_comment_sign('.m')
+    assert sign == '%'
+
+
+def test_ext_to_comment_sign_when_sign_not_in_dict():
+    with pytest.raises(CommentSignNotFound):
+        sign = comment_sign.ext_to_comment_sign('.qqq')
