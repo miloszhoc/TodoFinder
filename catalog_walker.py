b56d8a9dc29156b180a5162ab56ad879b7acd2d3
milosz.hoc <miloszhoc@gmail.com>
2019-09-15 01:01:21 +0200

    added encapsulation

--- catalog_walker.py
+++ catalog_walker.py
@@ -3,26 +3,27 @@ import sys
 
 
 class CatalogWalker:
-    def __init__(self, path, extension, excluded):
-        self.path_sep = '\\' if sys.platform == 'win32' else '/'
-        self.path = path + self.path_sep
-        self.files_in_path = self._get_files_from_path(path)
-        self.excluded = excluded if excluded is not None else ''
-        self.extension = extension
+    def __init__(self, main_path: str, extension: str, excluded: tuple):
+        self._path_sep = '\\' if sys.platform == 'win32' else '/'
+        self._path = main_path if main_path.endswith('\\') or \
+                                  main_path.endswith('/') else main_path + self._path_sep
+        self._files_in_path = self._get_files_from_path(main_path)
+        self._excluded = excluded if excluded is not None else ''
+        self._extension = extension
 
     def _get_files_from_path(self, p: str) -> list:
         return os.listdir(p)
 
     def _file_searcher(self, path, files: list) -> list:
         found_files = []
-        files = [path + file for file in files if file not in self.excluded]
+        files = [path + file for file in files if file not in self._excluded]
         for file in files:
             isdir = os.path.isdir(file)
             if isdir:
-                found_files.extend(self._file_searcher(file + self.path_sep, self._get_files_from_path(file)))
-            elif file.endswith('{}'.format(self.extension)):
+                found_files.extend(self._file_searcher(file + self._path_sep, self._get_files_from_path(file)))
+            elif file.endswith('{}'.format(self._extension)):
                 found_files.append(file)
         return found_files
 
-    def walk(self):
-        return self._file_searcher(self.path, self.files_in_path)
+    def walk(self) -> _file_searcher:
+        return self._file_searcher(self._path, self._files_in_path)
--- todo_searcher.py
+++ todo_searcher.py
@@ -2,18 +2,18 @@ import re
 
 
 class TodoSearcher:
-    def __init__(self, todo, fixme, comment_sign):
-        self.todo = todo
-        self.fixme = fixme
-        self.comment_sign = comment_sign
+    def __init__(self, todo: bool, fixme: bool, comment_sign: str):
+        self._todo = todo
+        self._fixme = fixme
+        self._comment_sign = comment_sign
 
-    def _set_regex(self, todo=False, fixme=False):
-        if todo and not fixme:
-            return '{} ?[Tt][Oo][Dd][Oo].+'.format(self.comment_sign)
-        elif fixme and not todo:
-            return '{} ?[Ff][Ii][Xx] ?[Mm][Ee].+'.format(self.comment_sign)
+    def _set_regex(self) -> str:
+        if self._todo and not self._fixme:
+            return '{} ?[Tt][Oo][Dd][Oo].+'.format(self._comment_sign)
+        elif self._fixme and not self._todo:
+            return '{} ?[Ff][Ii][Xx] ?[Mm][Ee].+'.format(self._comment_sign)
         else:
-            return '{} ?(([Tt][Oo][Dd][Oo].+)|([Ff][Ii][Xx][Mm][Ee].+))'.format(self.comment_sign)
+            return '{} ?(([Tt][Oo][Dd][Oo].+)|([Ff][Ii][Xx][Mm][Ee].+))'.format(self._comment_sign)
 
     def _finder(self, file_name: str, regex: str):
         with open(file_name, 'r', encoding='utf-8') as f:
@@ -24,6 +24,6 @@ class TodoSearcher:
                 if pattern:
                     print('line: ' + str(i) + ' ' + f.name + ' \n ' + pattern[0] + '\n')
 
-    def search(self, file_name):
-        regex = self._set_regex(self.todo, self.fixme)
+    def search(self, file_name: str):
+        regex = self._set_regex()
         self._finder(file_name, regex)
