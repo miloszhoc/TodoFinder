11cf972ae4eb108cb9c7c26e00ff409c17776dd4
milosz.hoc <miloszhoc@gmail.com>
2019-09-15 00:56:42 +0200

    more exceptions has been caught

--- api.py
+++ api.py
@@ -20,12 +20,16 @@ class Api:
     def find_in_catalog(ext: str, path, exclude, todo: bool, fixme: bool):
         try:
             comment_sign = ext_to_comment_sign(ext.lower())
-        except CommentSignNotFound as e:
-            print(e.message)
-        else:
             files = CatalogWalker(path, ext, exclude)
 
             searcher = TodoSearcher(todo, fixme, comment_sign)
 
+        except CommentSignNotFound as e:
+            print(e.message)
+        except FileNotFoundError:
+            print('Path does not exist')
+        except NotADirectoryError:
+            print('Not a directory')
+        else:
             for file in files.walk():
                 searcher.search(file)
