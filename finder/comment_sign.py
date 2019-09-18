9162b2bb0c4db5689e9025bb0c2a757388a91982
milosz.hoc <miloszhoc@gmail.com>
2019-09-18 20:04:57 +0200

    fixed extensions

--- finder/comment_sign.py
+++ finder/comment_sign.py
@@ -6,8 +6,8 @@ def ext_to_comment_sign(ext: str):
     signs = {('.py', '.sh'): '#',
              ('.java', '.c', '.cs', '.cpp', '.css', '.go', '.php', '.js'): '//',
              ('.xml', '.html', '.htm'): '<!--',
-             '.hs': '--',
-             '.m': '%'}
+             ('.hs',): '--',
+             ('.m',): '%'}
     for k, v in signs.items():
         if ext in k:
             return v
