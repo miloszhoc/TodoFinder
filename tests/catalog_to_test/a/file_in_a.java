654d562c516341e68e5e34b0736db4614c1d137b
milosz.hoc <miloszhoc@gmail.com>
2019-10-07 21:26:31 +0200

    added multiline comments to test file

--- tests/catalog_to_test/a/file_in_a.java
+++ tests/catalog_to_test/a/file_in_a.java
@@ -1,3 +1,20 @@
 //todo test1
 
 int a = 'test'; // fixme : test2
+
+/* todo find x
+fixme check this
+ todo add y
+ */
+public class X{
+    public static void main(String args[]){
+
+    }
+}
+
+/* todo repleace
+take x as parameter fixme x as integer
+test test
+ */
+
+// todo eof
