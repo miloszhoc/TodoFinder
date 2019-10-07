ba176ff2146d2faeb4d0d666f02975ba156f3cab
milosz.hoc <miloszhoc@gmail.com>
2019-10-07 21:14:14 +0200

    added support for multiline comments

--- finder/comment_sign.py
+++ finder/comment_sign.py
@@ -3,11 +3,15 @@ from exceptions.exceptions import *
 
 # todo: add more extensions
 def ext_to_comment_sign(ext: str):
-    signs = {('.py', '.sh'): '#',
-             ('.java', '.c', '.cs', '.cpp', '.css', '.go', '.php', '.js'): '//',
-             ('.xml', '.html', '.htm'): '<!--',
-             ('.hs',): '--',
-             ('.m',): '%'}
+    signs = {('.py', '.sh'): {'single': '#', 'multi': None},
+             ('.java', '.c', '.cs', '.cpp', '.css', '.go', '.php', '.js'): {'single': '//',
+                                                                            'multi': ('/*', '*/')},
+             ('.xml', '.html', '.htm'): {'single': '<!--',
+                                         'multi': ('<!--', '-->')},
+             ('.hs',): {'single': '--',
+                        'multi': None},
+             ('.m',): {'single': '%',
+                       'multi': ('%{', '%}')}}
     for k, v in signs.items():
         if ext in k:
             return v
--- tests/test_comment_sign.py
+++ tests/test_comment_sign.py
@@ -5,14 +5,14 @@ from exceptions.exceptions import *
 
 def test_ext_to_comment_sign_when_sign_is_in_dict_in_tuple():
     sign = comment_sign.ext_to_comment_sign('.c')
-    assert sign == '//'
+    assert sign == {'single': '//', 'multi': ('/*', '*/')}
 
 
 def test_ext_to_comment_sign_when_sign_is_in_dict_as_str():
     sign = comment_sign.ext_to_comment_sign('.m')
-    assert sign == '%'
+    assert sign == {'multi': ('%{', '%}'), 'single': '%'}
 
 
 def test_ext_to_comment_sign_when_sign_not_in_dict():
     with pytest.raises(CommentSignNotFound):
-        sign = comment_sign.ext_to_comment_sign('.qqq')
+        comment_sign.ext_to_comment_sign('.qqq')
